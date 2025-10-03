56b2601783ff1add14a3dc35db378a8b663b33d7
aproragadozo <aproragadozo@gmail.com>
2025-10-03 13:28:48 +0200

    added placeholder ideas for event handling in service worker

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
 console.log('Started', self);
+
+/*
+install: rendeld be a 100 fotó-url-t, ahogy eddig az app.js
+activate: clean up existing cache
+fetch: ha van elég a cache-ben, onnan szolgáld ki a játékot, és kérj be 100 - cacheméret mennyiségű új fotóurl-t
+    ellenőrizd, hogy szerepel-e bármelyik új kép a cache-ben, és ha igen, azok helyett is kérj újat
+*/
